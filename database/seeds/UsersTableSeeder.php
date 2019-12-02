34cea9f0359ab91327221dbe7a53b0679ffa5897
awais512 <raz.awais25@gmail.com>
2019-12-02 18:34:29 +0500

    Users Profile

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::where('email', 'awais@mail.com')->first();
+
+        if ($user) {
+            User::create([
+                'name' => 'Awais Ch',
+                'email' => 'awais@mail.com',
+                'password' => Hash::make('password')
+            ]);
+        }
+    }
+}
